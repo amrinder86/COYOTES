9a167b391bf9733e12f8d5942d3f11e1a36be50a
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-09 16:39:34 -0700

    Add another standalone method and test driver code

--- shout.rb
+++ shout.rb
@@ -1,5 +1,15 @@
 module Shout
+  
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yelling_happily(word)
+    puts "Hahaha ...#{word} ...this? Nice one."
+  end
 end
+#driver code 
+  puts Shout.yell_angrily("STOP")
+  puts Shout.yelling_happily("woah")
+
+
