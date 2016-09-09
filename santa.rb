23801878114de1d919db01527d81019073122ab8
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-08 19:02:20 -0700

    Test driver code

--- santa.rb
+++ santa.rb
@@ -17,3 +17,11 @@ class Santa
   end
 
 end
+
+#Driver code 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
