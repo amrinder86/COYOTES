92ed5793c1172a5f19c5c87cd0be1822b14dd20f
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-08 19:00:49 -0700

    Add intialize method and define instance variables

--- santa.rb
+++ santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-  def initialize
+  def initialize(gender,ethnicity)
+    @gender =gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
     puts "Initializing Santa instance ..."
+    puts "Gender: #{@gender} Ethnicity: #{ethnicity}"
   end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!" 
@@ -10,3 +15,5 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts  "That was a good #{cookie_type}!" 
   end
+
+end
