5d619911ddc562491cd96533097d53e163bcda33
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-08 19:48:36 -0700

    Add getter and setter methods along with 3 instance methods

--- santa.rb
+++ santa.rb
@@ -8,12 +8,37 @@ class Santa
     puts "Initializing Santa instance ..."
     puts "Gender: #{@gender} Ethnicity: #{ethnicity}"
   end
+
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!" 
   end
 
   def eat_milk_and_cookies(cookie_type)
     puts  "That was a good #{cookie_type}!" 
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    puts "#{reindeer_name} you don't deserve to be on this list at the moment #{@reindeer_ranking}" 
+    @reindeer_ranking << reindeer_name
+    puts "Santa gets mad at #{reindeer_name} and  places #{reindeer_name} in last place ranking in #{@reindeer_ranking}"
+    end
+    
+  #setter methods
+  def gender=(new_gender)
+      @gender = new_gender
+  end
+  # getter methods 
+  def age
+    @age
+  end
+  
+  def ethnicity
+    @ethnicity
   end
 
 end
