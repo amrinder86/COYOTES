1db71af3a1e1f8824fd4cc82d4168a5cad90cdba
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-12 06:20:02 -0700

    Cleaned code - finish with solo challenge

--- guess_word_game.rb
+++ guess_word_game.rb
@@ -24,22 +24,26 @@
 
   class Guess_word
     attr_reader :guess_count,:length, :user_display_array,:provided_word
+    
     def initialize(provided_word)
       @provided_word = provided_word
-   p   @length = provided_word.length
-   p   @user_display_array = Array.new(@length,"_")
-   p   @guess_count = @length
+      @length = provided_word.length
+      @user_display_array = Array.new(@length,"_")
+      @guess_count = @length
     end
 
     def word_check_logic(letter)
+
       if @provided_word.include?(letter)
+        puts "Awesome.Word you trying to solve does contain letter '#{letter}'"
+        puts""
           @user_display_array.delete_at(@provided_word.index(letter))
             @user_display_array.insert(@provided_word.index(letter), letter)
              @user_display_array.join(' ')
              #@user_display_array
       else 
         @guess_count -= 1
-        puts"try again"
+        puts"Please try again.Word you trying to solve does have letter '#{letter}' in it."
           @user_display_array.join(' ')
          #p @user_display_array
       end
@@ -48,51 +52,67 @@
   end
 
   #driver code
-puts "word"
-  given_word= gets.chomp
-  game = Guess_word.new(given_word)
-  loop do
-  puts"make a guess"
-  puts "you have #{game.guess_count} tries left to guess the word."
-  user_guess=gets.chomp
-  game.word_check_logic(user_guess)
-  p final_word=game.user_display_array.join(' ')
-  puts "this is the word so far #{final_word}"
-  
-  puts "to solve the Puzzle type 'yes'  or press any other key to continue"
-  puzzle_solve=gets.chomp
-  if puzzle_solve == 'yes'
-    puts "Go ahead"
-    respone=gets.chomp
-    if respone == given_word 
-    puts "you win"
-    break
-    end
-    elsif game.guess_count == 0
-    puts "YOu lost..you ran out of tries."
-  else
-    puts "As you wish ..keep trying to guess"
-  end
+  puts "Name of 1st player:"
+    player1_name=gets.chomp
+  puts "Name of 2nd player:"
+    player2_name=gets.chomp
 
-  # if final_word == given_word # not working at the moment unless I ask for a whole string from user at once
-  #   puts "you win"
-  #   break
-  # end
-  #if @user_display_array == given_word
-  # puts "you won"
-  # break
-  # elsif @guess_count == 0
-  #   puts "you ran out of tries"
-  #   break
-    #else
-  #puts"try again"
-  #end
-  break if game.guess_count == 0
-  
+  puts "#{player1_name} please type the word you want #{player2_name} to guess."
+      given_word= gets.chomp
+
+  puts "#{player2_name} now it's your turn to guess the word."
+    game = Guess_word.new(given_word)
+      
+    loop do
+      puts""
+      puts"Please write a letter to see if word contains your letter."
+      puts ""
+      puts "You have #{game.guess_count} tries left to guess the word."
+        user_guess=gets.chomp
+        game.word_check_logic(user_guess)
+        p final_word=game.user_display_array.join(' ')
+        
+        puts "This is the word so far #{final_word}"
+        puts""
+        
+        puts "To solve the Puzzle type 'yes'  or press any other key to continue"
+          puzzle_solve=gets.chomp
+            if puzzle_solve == 'yes'
+                puts "Go ahead"
+                  respone=gets.chomp
+                    if respone == given_word 
+                      puts "Fantastic job!!!! You Won!!!!!"
+                      puts "************************************"
+                      puts "Achievement Unlocked : Smart Cookie"
+                      puts "************************************"
+                    break
+                    end
+                    elsif game.guess_count == 0
+                      puts "Sorry you lost.You ran out of tries."
+                      puts "************************************"
+                      puts "**************LOSER*****************"
+                      puts "************************************"
+                    else
+                      puts "Okay as you wish ..keep trying to guess"
+            end
+
+        # if final_word == given_word # not working at the moment unless I ask for a whole string from user at once
+        #   puts "you win"
+        #   break
+        # end
+        # elsif @guess_count == 0
+        #   puts "you ran out of tries"
+        #   break
+        #else
+        #puts"try again"
+        #end
+        break if game.guess_count == 0
+        
     
   end
   
-  puts "YOu lost..you ran out of tries."
+  puts""
+  puts "Thanks for playing.Enjoy your day. "
   
 
 
