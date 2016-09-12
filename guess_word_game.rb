09d7ebdb54726564ea52dfa0d365e79df2f2e0a3
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-12 02:49:59 -0700

    Intial commit - pseudocode

---
+++ guess_word_game.rb
@@ -0,0 +1,27 @@
+#pseudocode
+# Create a word guessing game that two users can play
+# first user provides the word 
+  # second user tries to guess it.
+# print dashes to screen and multiply it by the length of word that first user provided.
+# set the guess limit to length of the word that first user provided
+
+# now when second user starts guessing the word
+  # tell the user how many tries they have 
+  #second user will provide one letter at a time.
+# check to see if that letter is included in word
+      # if it is included then take that word and store it 
+        # otherwise tell the user try agian it's not included in the word.
+        # now if it included check that letter's index and replace it in dashes with the same index.
+
+# once the user guesses the word or runs out of tries do this.
+     # if the guessed word matches with provided 
+          # write a victory msg and end the game.
+      #otherwise
+          # tell the second user they lost because they ran out of tries.
+            #end the game 
+            
+
+
+
+
+
