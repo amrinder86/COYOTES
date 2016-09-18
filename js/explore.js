e89d7fc7c8bc43729c06cd17b0c88b816d10677e
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-18 16:53:51 -0700

    Write a functions the reverses the string

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,18 @@
 // loop will run until the values if counter becomes zero 
 // we will use i-- to Decrement our counter by 1 so it goes from 
 // string's length to zero
+
+function reverse(string) {
+  
+  var new_string = '';
+  
+  for (var i = string.length - 1; i >= 0; i--)
+    
+    new_string += string[i];
+  
+  return new_string;
+}
+
+ // reverse("I'm loving javascript so far")
+
+console.log(reverse("hello"));
