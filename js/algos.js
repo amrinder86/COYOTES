089558a2892a9b60084f531dc026617a853d85d5
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-18 17:53:26 -0700

    Create algos.js and pseudocode to find the longest word in array

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+// create a function that takes array as parameter
+// create a empty variable 
+// then iterate through the whole array and stores each word/phrase and compared each word's length to that stored variable
+// if the length of that stored word is shorter than the one it got compared to update the stored variable
+// Every time the compared word is bigger in length than stored word ..update the stored word..
+// print the stored word as it'll be the longest word/phrase in length.
