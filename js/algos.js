8bbb86e47fddec5c8108c8d14d6f737149ab6619
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-18 17:58:48 -0700

    Create a function to find longest word in array and test with driver code

--- js/algos.js
+++ js/algos.js
@@ -4,3 +4,19 @@
 // if the length of that stored word is shorter than the one it got compared to update the stored variable
 // Every time the compared word is bigger in length than stored word ..update the stored word..
 // print the stored word as it'll be the longest word/phrase in length.
+
+function longest_phrase(array){
+      var longest_word_so_far = "";
+          for(var i = 0; i < (array.length); i++){
+            if (array[i].length > longest_word_so_far.length){
+                longest_word_so_far = array[i];
+    }
+  }
+      console.log("Longest word or phrase in the array is  -->> " +longest_word_so_far);
+  }
+ 
+  // driver code 
+  var array1 = ["longest","longer","long", "this is long","this is very very long"];
+  var array2 = ["long phrase","longest phrase","longer phrase"];
+  longest_phrase(array1);
+  longest_phrase(array2);
